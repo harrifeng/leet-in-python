3ab6b4d364ae54365cf76ca64f4313ccd6a300eb
Harri Feng <harrifeng@gmail.com>
2016-02-28 19:27:27 +0800

    new accepted

---
+++ 007_reverse_integer.py
@@ -0,0 +1,38 @@
+"""
+Reverse digits of an integer.
+  Example1: x = 123, return 321
+  Example2: x = -123, return -321
+"""
+
+import unittest
+
+
+class MyTest(unittest.TestCase):
+
+    def test(self):
+        solution = Solution()
+        self.assertEqual(123, solution.reverse(321))
+        self.assertEqual(-123, solution.reverse(-321))
+        self.assertEqual(0, solution.reverse(2**31))
+
+
+class Solution(object):
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        pos = True
+        if x < 0:
+            pos = False
+        x = abs(x)
+        ret = 0
+        while x > 0:
+            ret = ret * 10 + x % 10
+            x //= 10
+        if ret > 2**31 - 1:
+            return 0
+        if not pos:
+            return -1 * ret
+        return ret
