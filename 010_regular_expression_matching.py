4fa14248a25a8bbacc14bdfdcd91b8d84ec31901
Harri Feng <harrifeng@gmail.com>
2016-03-05 16:52:36 +0800

    new accepted

--- 010_regular_expression_matching.py
+++ 010_regular_expression_matching.py
@@ -33,24 +33,24 @@ class MyTest(unittest.TestCase):
         self.assertFalse(solution.isMatch("aaa", "aa"))
 
 
-class Solution(object):
+class Solution:
+    # @return a boolean
 
     def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
-        if len(p) == 0:
-            return len(s) == 0
-        if len(p) == 1:
-            return len(s) == 1 and (p[0] == '.' or p[0] == s[0])
-        if p[1] == '*':
-            if self.isMatch(s, p[2:]):
-                return True
-            else:
-                return (len(s) > 0 and (p[0] == '.' or s[0] == p[0]) and
-                        self.isMatch(s[1:], p))
-        else:
-            return (len(s) > 0 and (p[0] == '.' or s[0] == p[0]) and
-                    self.isMatch(s[1:], p[1:]))
+        dp = [[False for i in range(len(p) + 1)] for j in range(len(s) + 1)]
+        dp[0][0] = True
+        for i in range(1, len(p) + 1):
+            if p[i - 1] == '*':
+                if i >= 2:
+                    dp[0][i] = dp[0][i - 2]
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 1] or dp[i][j - 2] or (
+                        dp[i - 1][j] and (s[i - 1] == p[j - 2] or
+                                          p[j - 2] == '.'))
+                else:
+                    dp[i][j] = dp[i - 1][j - 1] and s[i - 1] == p[j - 1]
+        return dp[len(s)][len(p)]
