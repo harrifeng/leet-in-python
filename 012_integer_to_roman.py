c528ff55ee264805e65e1603885d9958e9379060
Haoran Feng <haoran.feng@sap.com>
2016-03-07 15:27:45 +0800

    new accepted

--- 012_integer_to_roman.py
+++ 012_integer_to_roman.py
@@ -1,7 +1,6 @@
 """
 Given an integer, convert it to a roman numeral.
 Input is guaranteed to be within the range from 1 to 3999.
-Subscribe to see which companies asked this question
 """
 
 import unittest
