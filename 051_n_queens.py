115599518ad9addcb87e97f70a72ae2cdc26c444
Harri Feng <harrifeng@gmail.com>
2016-03-11 22:49:50 +0800

    complicated, but easy understandable version

--- 051_n_queens.py
+++ 051_n_queens.py
@@ -51,4 +51,26 @@ class Solution(object):
         :type n: int
         :rtype: List[List[str]]
         """
-        return [["Q"]]
+        ret = []
+        tmp = ['.' * n for i in range(n)]
+        self.helper(n, 0, tmp, ret)
+        return ret
+
+    def helper(self, n, level, tmp, ret):
+        if level == n:
+            ret.append(tmp[:])
+            return
+        for i in range(n):
+            new_row = '.' * n
+            tmp[level] = new_row[:i] + 'Q' + new_row[i + 1:]
+            if self.is_valid(tmp, level, i):
+                self.helper(n, level + 1, tmp, ret)
+            tmp[level] = new_row
+
+    def is_valid(self, board, row, col):
+        for i in range(row):
+            for j in range(len(board[0])):
+                if board[i][j] == 'Q' and (j == col or
+                                           abs(row - i) == abs(col - j)):
+                    return False
+        return True
