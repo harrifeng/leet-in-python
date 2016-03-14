b7be893f3c3265b75184f5d42a3dd9708154ce9e
Haoran Feng <haoran.feng@sap.com>
2016-03-14 12:05:14 +0800

    new accepted

--- 029_divide_two_integers.py
+++ 029_divide_two_integers.py
@@ -11,17 +11,38 @@ class MyTest(unittest.TestCase):
     def test(self):
         solution = Solution()
         self.assertEqual(5, solution.divide(15, 3))
+        self.assertEqual(5000, solution.divide(15000, 3))
 
 
 class Solution(object):
+
     def divide(self, dividend, divisor):
         """
         :type dividend: int
         :type divisor: int
         :rtype: int
         """
-        cnt = 0
+        if (dividend < 0) != (divisor < 0):
+            sign = -1
+        else:
+            sign = 1
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        ret = 0
         while dividend >= divisor:
-            dividend -= divisor
-            cnt += 1
-        return cnt
+            tmp = divisor
+            i = 0
+            while dividend >= tmp:
+                dividend -= tmp
+                ret += (1 << i)
+                i += 1
+                tmp = (tmp << 1)
+
+        imin, imax = -2 ** 31, 2 ** 31 - 1
+        if ret * sign > imax:
+            return imax
+        if ret * sign < imin:
+            return imin
+        return ret * sign
