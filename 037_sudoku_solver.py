92d0466c1b671ca2f720be7176610fe5b4dc4099
Harri Feng <harrifeng@gmail.com>
2016-03-19 12:32:18 +0800

    accepted

--- 037_sudoku_solver.py
+++ 037_sudoku_solver.py
@@ -11,46 +11,53 @@ class MyTest(unittest.TestCase):
 
     def test(self):
         solution = Solution()
-        a1 = ["..9748...", "7........", ".2.1.9...", "..7...24.",
-              ".64.1.59.", ".98...3..", "...8.3.2.", "........6", "...2759.."]
-        r1 = ["519748632", "783652419", "426139875", "357986241",
-              "264317598", "198524367", "975863124", "832491756", "641275983"]
+        pa1 = ["..9748...", "7........", ".2.1.9...", "..7...24.",
+               ".64.1.59.", ".98...3..", "...8.3.2.", "........6", "...2759.."]
+        a1 = []
+        for i in pa1:
+            a1.append(list(i))
+
+        pr1 = ["519748632", "783652419", "426139875", "357986241",
+               "264317598", "198524367", "975863124", "832491756", "641275983"]
+        r1 = []
+        for i in pr1:
+            r1.append(list(i))
+
         solution.solveSudoku(a1)
         self.assertEqual(r1, a1)
 
 
-class Solution(object):
+class Solution:
+    # @param board, a 9x9 2D array
+    # Solve the Sudoku by modifying the input board in-place.
+    # Do not return any value.
 
     def solveSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: void Do not return anything, modify board in-place instead.
-        """
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == '.':
-                    for k in '123456789':
-                        board[i] = board[i][:j] + k + board[i][j + 1:]
-                        if (self.isValid(board, i, j) and
-                                self.solveSudoku(board)):
-                            return True
-                        board[i] = board[i][:j] + '.' + board[i][j + 1:]
+        def isValid(x, y):
+            tmp = board[x][y]
+            board[x][y] = 'D'
+            for i in range(9):
+                if board[i][y] == tmp:
                     return False
-        return True
-
-    def isValid(self, board, x, y):
-        for i in range(9):
-            if i != x and board[i][y] == board[x][y]:
-                return False
-        for j in range(9):
-            if j != y and board[x][j] == board[x][y]:
-                return False
-
-        a = x / 3 * 3
-        b = y / 3 * 3
-
-        for i in range(a, a + 3):
-            for j in range(b, b + 3):
-                if (i != x or j != y) and board[i][j] == board[x][y]:
+            for i in range(9):
+                if board[x][i] == tmp:
                     return False
-        return True
+            for i in range(3):
+                for j in range(3):
+                    if board[(x / 3) * 3 + i][(y / 3) * 3 + j] == tmp:
+                        return False
+            board[x][y] = tmp
+            return True
+
+        def dfs(board):
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j] == '.':
+                        for k in '123456789':
+                            board[i][j] = k
+                            if isValid(i, j) and dfs(board):
+                                return True
+                            board[i][j] = '.'
+                        return False
+            return True
+        dfs(board)
