eb09d7e9d73993989f74a19ddd419805c71e21ea
Haoran Feng <haoran.feng@sap.com>
2016-03-24 15:51:06 +0800

    new test case

--- 045_jump_game_ii.py
+++ 045_jump_game_ii.py
@@ -17,6 +17,10 @@ class MyTest(unittest.TestCase):
     def test(self):
         solution = Solution()
         self.assertEqual(2, solution.jump([2, 3, 1, 1, 4]))
+        self.assertEqual(5, solution.jump([5, 6, 4, 4, 6, 9, 4, 4, 7, 4, 4,
+                                           8, 2, 6, 8, 1, 5, 9, 6, 5, 2, 7,
+                                           9, 7, 9, 6, 9, 4, 1, 6, 8, 8, 4,
+                                           4, 2, 0, 3, 8, 5]))
 
 
 class Solution(object):
