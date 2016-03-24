6889fae2423f14bb62f2d1d4bc3c58e7f4ecd8f7
Haoran Feng <haoran.feng@sap.com>
2016-03-24 14:10:29 +0800

    my accepted like regular expression

--- 044_wildcard_matching.py
+++ 044_wildcard_matching.py
@@ -51,8 +51,11 @@ class Solution(object):
 
         for i in range(1, len(s) + 1):
             for j in range(1, len(p) + 1):
-                if p[j - 1] == '*':
-                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
-                elif p[j - 1] == '?' or s[i - 1] == p[j - 1]:
+                if p[j - 1] == '?':
                     dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+                else:
+                    dp[i][j] = dp[i - 1][j - 1] and s[i - 1] == p[j - 1]
+
         return dp[len(s)][len(p)]
