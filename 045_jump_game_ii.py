b8c90d3797a4fc899763fdad6661717622138dc8
Haoran Feng <haoran.feng@sap.com>
2016-03-24 14:41:53 +0800

    their o(n) solution!!!

--- 045_jump_game_ii.py
+++ 045_jump_game_ii.py
@@ -16,22 +16,22 @@ class MyTest(unittest.TestCase):
 
     def test(self):
         solution = Solution()
-        # self.assertEqual(2, solution.jump([2, 3, 1, 1, 4]))
-        self.assertEqual(2, solution.jump_dp([2, 3, 1, 1, 4]))
+        self.assertEqual(2, solution.jump([2, 3, 1, 1, 4]))
 
 
 class Solution(object):
 
-    def jump_dp(self, nums):
+    def jump(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        N = len(nums)
-        dp = [N for i in range(N)]
-        dp[0] = 0
-        for i in range(N):
-            for j in range(i)[::-1]:
-                if nums[j] + j >= i:
-                    dp[i] = min(dp[i], dp[j] + 1)
-        return dp[N - 1]
+        ret = 0
+        last = 0
+        cur = 0
+        for i, c in enumerate(nums):
+            if i > last:
+                last = cur
+                ret += 1
+            cur = max(cur, i + c)
+        return ret
