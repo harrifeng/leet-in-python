ab74e530647d1f749e8f0e9b8034be8d5172063e
Harri Feng <harrifeng@gmail.com>
2016-04-10 15:04:59 +0800

    More reasonable, because only from the second, could it use older result

--- 010_regular_expression_matching.py
+++ 010_regular_expression_matching.py
@@ -39,8 +39,8 @@ class Solution:
     def isMatch(self, s, p):
         dp = [[False for i in range(len(p) + 1)] for j in range(len(s) + 1)]
         dp[0][0] = True
-        for i in range(1, len(p) + 1):
-            if i >= 2 and p[i - 1] == '*':
+        for i in range(2, len(p) + 1):
+            if p[i - 1] == '*':
                 dp[0][i] = dp[0][i - 2]
         for i in range(1, len(s) + 1):
             for j in range(1, len(p) + 1):
