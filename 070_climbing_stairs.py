05ecc3f4e7c73df22a577042ae906c62015c47b5
Harri Feng <harrifeng@gmail.com>
2016-04-10 13:43:48 +0800

    their accepted

--- 070_climbing_stairs.py
+++ 070_climbing_stairs.py
@@ -20,16 +20,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
+        if n <= 1:
+            return n
 
-        a = 1
-        b = 2
-        c = a + b
-        for i in range(n - 2):
-            c = a + b
-            a = b
-            b = c
-        return c
+        dp = [0 for i in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n - 1]
