6c4344d44daceeeb809e7e9b6f83a74d6e29018e
Haoran Feng <haoran.feng@sap.com>
2016-04-11 17:20:36 +0800

    more productive

--- 062_unique_paths.py
+++ 062_unique_paths.py
@@ -24,13 +24,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [[0 for i in range(n)] for j in range(m)]
-
-        for i in range(m):
-            dp[i][0] = 1
-
-        for i in range(n):
-            dp[0][i] = 1
+        dp = [[1 for i in range(n)] for j in range(m)]
 
         for i in range(1, m):
             for j in range(1, n):
