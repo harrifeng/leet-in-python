78c95002ea831571124610b590f5de110d72476a
Harri Feng <harrifeng@gmail.com>
2016-04-16 11:46:24 +0800

    my rework

--- 065_valid_number.py
+++ 065_valid_number.py
@@ -23,37 +23,40 @@ class MyTest(unittest.TestCase):
         self.assertTrue(solution.isNumber("2e10"))
 
 
-class Solution:
-    # @param s, a string
-    # @return a boolean
-    # @finite automation
+class Solution(object):
 
     def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         s = s.strip()
         s += '#'
         i = 0
         n1, n2, n3 = 0, 0, 0
         if s[i] in ['+', '-']:
             i += 1
+
         while s[i].isdigit():
-            n1 += 1
             i += 1
+            n1 += 1
+
         if s[i] == '.':
             i += 1
 
         while s[i].isdigit():
-            n2 += 1
             i += 1
-        if n1 == n2 == 0:       # e has something on left
-            return False
+            n2 += 1
+        if n1 == n2 == 0:
+            return False  # e has something on left
+
         if s[i] in ['e', 'E']:
             i += 1
             if s[i] in ('+', '-'):
                 i += 1
-            n3 = 0
             while s[i].isdigit():
-                n3 += 1
                 i += 1
+                n3 += 1
             if n3 == 0:
-                return False    # e has something on right
+                return False  # e has something on right
         return s[i:] == '#'
