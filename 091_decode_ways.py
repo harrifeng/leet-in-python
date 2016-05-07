f36e9f783bacf88f8e2c7f0f91c9bb02b0046855
Harri Feng <harrifeng@gmail.com>
2016-05-07 15:27:37 +0800

    my accepted

--- 091_decode_ways.py
+++ 091_decode_ways.py
@@ -40,21 +40,27 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if s == '' or s[0] == '0':
-            return 0
-        dp = [1, 1]
         N = len(s)
+        if N == 0:
+            return 0
+        if s[0] == '0':
+            return 0
+
+        dp = [0] * (N + 1)
+        dp[0] = 1
+        dp[1] = 1
 
         for i in range(2, N + 1):
-            if 10 <= int(s[i - 2:i]) <= 26:
-                if s[i - 1] != '0':
-                    dp.append(dp[i - 1] + dp[i - 2])
+            if s[i - 1] == '0':
+                if s[i - 2] == '1' or s[i - 2] == '2':
+                    dp[i] = dp[i - 2]
                 else:
-                    dp.append(dp[i - 2])
-            # last two is not valid, if last is '0'
-            # no futher is needed to process
-            elif s[i - 1] == '0':
-                return 0
+                    dp[i] = 0
             else:
-                dp.append(dp[i - 1])
+                if s[i - 2] == '0':
+                    dp[i] = dp[i - 1]
+                elif int(s[i - 2:i]) > 10 and int(s[i - 2:i]) <= 26:
+                    dp[i] = dp[i - 2] + dp[i - 1]
+                else:
+                    dp[i] = dp[i - 1]
         return dp[N]
