2aca734df47923df5ba9000135477b7769566ffa
Harri Feng <harrifeng@gmail.com>
2016-05-07 14:04:39 +0800

    shorter

--- 088_merge_sorted_array.py
+++ 088_merge_sorted_array.py
@@ -33,20 +33,19 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
-        i = m - 1
-        j = n - 1
-        x = m + n - 1
+        p = m + n - 1
+        m -= 1
+        n -= 1
 
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[x] = nums1[i]
-                i -= 1
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[p] = nums1[m]
+                m -= 1
             else:
-                nums1[x] = nums2[j]
-                j -= 1
-            x -= 1
-        while j >= 0:
-            nums1[x] = nums2[j]
-            x -= 1
-            j -= 1
+                nums1[p] = nums2[n]
+                n -= 1
+            p -= 1
+        while n >= 0:
+            nums1[n] = nums2[n]
+            n -= 1
         return
