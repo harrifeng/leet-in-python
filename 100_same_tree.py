38781117ed5ed0b484a0059091b5fb758c69ee5e
Harri Feng <harrifeng@gmail.com>
2016-05-07 11:59:01 +0800

    reformat

--- 100_same_tree.py
+++ 100_same_tree.py
@@ -41,4 +41,6 @@ class Solution(object):
             return True
         if p is None or q is None:
             return False
-        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return (p.val == q.val) and \
+            self.isSameTree(p.left, q.left) and \
+            self.isSameTree(p.right, q.right)
