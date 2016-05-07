6e810c0c14bdd41aaa1c5eaf47f00363dc23983a
Harri Feng <harrifeng@gmail.com>
2016-05-07 13:53:39 +0800

    even shorter

--- 096_unique_binary_search_trees.py
+++ 096_unique_binary_search_trees.py
@@ -30,13 +30,10 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n <= 1:
-            return 1
         dp = [0] * (n + 1)
         dp[0] = 1
-        dp[1] = 1
 
-        for i in range(2, n + 1):
+        for i in range(1, n + 1):
             for j in range(i):
                 dp[i] += dp[j] * dp[i - 1 - j]
         return dp[n]
